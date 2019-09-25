dfaf1ed0d941a1ebb4fa595df718cf8d310cd003
Saint Kim <dragor@hanmail.net>
2019-09-25 17:44:34 +0900

    레인 배정 저장

--- js/base.js
+++ js/base.js
@@ -10,3 +10,22 @@ function doPush(){
      objArray.push(objItem.value);
      printArray();
 }
+function gen() {
+    n1 = Math.floor(Math.random()*1);
+    f.random.value = n1;
+}
+function laneData(){ 
+    var nickname = new Array(); 
+        // nickname[0]="꿈꾸는나무"; 
+        // nickname[1]="사과"; 
+        // nickname[2]="배"; 
+        // nickname[3]="포도"; 
+        
+        nickname="200";
+        nickname="나무";
+
+var lane = Math.floor(Math.random()*(nickname.length)); 
+
+document.write(nickname[lane]); 
+alert(laneData()); 
+} 
